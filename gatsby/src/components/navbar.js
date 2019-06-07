c12d5bd71f6718fab7d13ceb362f1b6af428d97a
smanierre <sean.manierre@gmail.com>
2019-06-06 22:24:12 -0400

    Working more on getting this navbar right

--- gatsby/src/components/navbar.js
+++ gatsby/src/components/navbar.js
@@ -2,27 +2,39 @@ import React from "react"
 import styled from "styled-components"
 
 const Wrapper = styled.nav`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   height: 7vh;
   width: 100%;
-  background-color: lightcoral;
+  background-color: lightblue;
   display: flex;
   justify-content: center;
+
+  & *:not(:last-child)::after {
+    content: "";
+    display: inline-block;
+    height: 75%;
+    border-right: 1px solid black;
+  }
+
+  & *:hover {
+    background-color: black;
+    color: white;
+  }
 `
 
 export const Logo = styled.div`
+  height: 100%;
   text-align: center;
   color: black;
-  padding: 25px;
-  border-right: 1px solid black;
-  border-left: 1px solid black;
+  padding: 0 25px;
 `
 
 export const NavItem = styled.a`
   display: inline-block;
-  padding: 0 25px
+  height: 100%;
+  padding: 0 25px;
   text-align: center;
 `
 
