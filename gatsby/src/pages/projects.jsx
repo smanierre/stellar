705fd4a1cc0c570a4f50b5b83bdee98f05d9f79b
Sean Manierre <sean@pop-os.localdomain>
2019-07-23 06:58:17 -0400

    Starting work on the projects page. Got the data that is needed for the project thumbnails, now just need to display it correctly.

--- gatsby/src/pages/projects.jsx
+++ gatsby/src/pages/projects.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { graphql } from "gatsby"
+import PropTypes from "prop-types"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Background from "../components/utilities/background"
+import Projects from "../components/projects/projects-content"
 
-const ProjectPage = () => (
+const ProjectPage = ({ data }) => (
   <Background>
     <Layout>
       <SEO
@@ -23,8 +26,34 @@ const ProjectPage = () => (
           "whelen",
         ]}
       />
+      <Projects data={data} />
     </Layout>
   </Background>
 )
 
 export default ProjectPage
+
+export const pageQuery = graphql`
+  query {
+    allSanityProject {
+      nodes {
+        title
+        thumbnail {
+          asset {
+            fixed(height: 100, width: 100) {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+ProjectPage.defaultProps = {
+  data: {},
+}
+
+ProjectPage.propTypes = {
+  data: PropTypes.instanceOf(Object),
+}
